3a366fc1c4b417ea9983dd0d78da41a2e533ba1e
4eug <debraheug@gmail.com>
2022-11-27 09:55:26 +0000

    Index

--- pages/index.js
+++ pages/index.js
@@ -1,11 +1,10 @@
+import Login from "../components/login";
 
 
 export default function Home() {
   return (
     <>
-      <div className="text-7xl text-blue-700">
-        Lets Start
-      </div>
+      <Login/>
     </>
   );
 }
